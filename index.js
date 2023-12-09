439c4cfbc122bab93207f642ccb81bbd8b8a5ae2
nksajwani <narendra@macha.ai>
2023-12-09 20:35:36 +0530

    port change

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const userRouter = require("./routes/userRoutes.js");
 const ipfsRouter = require("./routes/ipfsRoutes.js");
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
